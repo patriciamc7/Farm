622a321e4845905862ed28813e9cb80a76caf1b5
Patri <patricia.martinez.cruz.pmn@gmail.com>
2021-07-15 03:22:14 +0200

    start

---
+++ Assets/Scripts/Day_time.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Day_time : MonoBehaviour
+{
+    public GameObject light_sun;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light();
+    }
+    void light() //Change the color of light as the sun goes down
+    {
+        Light lightComp = light_sun.GetComponent<Light>();
+        float H, S, V, r, g, b;
+        Color.RGBToHSV(lightComp.color, out H, out S, out V);
+        H -= Time.deltaTime * 0.01f; //change the factor, the factor is the time the day lasts
+        lightComp.color = Color.HSVToRGB(H, S, V);
+        //if (H < 235 && go to sleep)
+        //    H = 63;
+        //if (H < 235 && hora igual)
+        //    H = 235;
+    }
+}
